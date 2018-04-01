d983876d64754b18781a0a2cef12c2a5ea2166e5
eduardokreve <eduardokreve283@gmail.com>
2018-03-31 23:19:02 -0300

    Ja deu sono

--- Trab.py
+++ Trab.py
@@ -1,29 +1,37 @@
 # coding=utf-8
+import string
 
 #função que cria a tabela 
 def criaTable(tab):
-    print("deu certo ")
+    test = tab.split("create table")
+    print(test)  
+      
 
 
+
+
+    
+
+#função que insere valores na tabela
+def inseTable(valor):
+    print("funcao que insere os valores")
 
 
 def main():
 
     while(1): 
-        comand = input("Comando SQL: ")
+        comand = input("Comando SQL:")
 
         #verifica os comandos SQL digitados corretamente 
         if(comand.find("create") != -1 and comand.find("table") != -1): 
             criaTable(comand)
 
         elif(comand.find("insert") != -1 and comand.find("into") != -1):
-            
-        
+            inseTable(comand)
+
         else:
             print("Comando incorreto")
             break
-
-
 
 if __name__ == "__main__":
     main()
