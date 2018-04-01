675b6cf75c71a115317b71d767814d8661569f25
eduardokreve <eduardokreve283@gmail.com>
2018-03-31 23:59:04 -0300

    Trabalhando com listas

--- Trab.py
+++ Trab.py
@@ -2,15 +2,14 @@
 import string
 
 #função que cria a tabela 
-def criaTable(tab):
-    test = tab.split("create table")
-    print(test)  
+def criaTable(tab):  
+    nameTable = list(map(str, tab.split())) #converte o argumento para uma lista
+
+    del nameTable[0:2]#exclui create e table da lista
+    
+    print(nameTable)
       
 
-
-
-
-    
 
 #função que insere valores na tabela
 def inseTable(valor):
@@ -20,10 +19,10 @@ def inseTable(valor):
 def main():
 
     while(1): 
-        comand = input("Comando SQL:")
-
+        comand = input("Comando SQL:")#a entrada é str
+        
         #verifica os comandos SQL digitados corretamente 
-        if(comand.find("create") != -1 and comand.find("table") != -1): 
+        if(comand.find("create ") != -1 and comand.find("table ") != -1): 
             criaTable(comand)
 
         elif(comand.find("insert") != -1 and comand.find("into") != -1):
