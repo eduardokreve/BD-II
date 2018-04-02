a74a7e8e16851b88b37c0e12e9b5c8c7c990d327
eduardokreve <eduardokreve283@gmail.com>
2018-04-01 22:59:15 -0300

    Limpeza dos comandos

--- Trab.py
+++ Trab.py
@@ -1,24 +1,38 @@
 # coding=utf-8
 import string
+import re
 
 #função que cria a tabela 
 def criaTable(tab):  
-    nameTable = list(map(str, tab.split())) #converte o argumento para uma lista
+    table = list(map(str, tab.split()))#converte o argumento para uma lista
+    del table[0:2]#exclui create e table da lista
 
-    del nameTable[0:2]#exclui create e table da lista
+    nameTable = table[0:1]#pega o nome da tabela
+
+    #faz a limpeza do comando ficando somente o atributo e tipo
+    del table[0:1]
+    del table[0:1]
+    del table[len(table) -1 : len(table)] 
     
-    print(nameTable)
-      
-
-
+    while(1): #tira todas s ','
+        try:
+            del table[table.index(',')]
+        except ValueError:
+            break
+   
+    print(table)
+    
 #função que insere valores na tabela
 def inseTable(valor):
     print("funcao que insere os valores")
 
 
 def main():
+    print("Insira espaço entre ',' e parenteses")
+    print("Ex.: create table dados ( id int , letra char )\n\n")
 
     while(1): 
+        
         comand = input("Comando SQL:")#a entrada é str
         
         #verifica os comandos SQL digitados corretamente 
